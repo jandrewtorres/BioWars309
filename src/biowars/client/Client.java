c765e8b5ce0092dceab612c4fa9a8e2760cca82c
John Torres <jandrewtorres5@gmail.com>
2018-01-13 17:36:58 -0800

    Ticket #1. Created Eclipse project. Created server and client packages. Created main Server.java and Client.java classes. Created run_configs/ folder and added the server and client run configs.

---
+++ src/biowars/client/Client.java
@@ -0,0 +1,9 @@
+package biowars.client;
+
+public class Client {
+
+	public static void main(String[] args) {
+		System.out.println("Im the client!");
+	}
+
+}
---
+++ src/biowars/server/Server.java
@@ -0,0 +1,9 @@
+package biowars.server;
+
+public class Server {
+
+	public static void main(String[] args) {
+		System.out.println("Im the server!");
+	}
+
+}
