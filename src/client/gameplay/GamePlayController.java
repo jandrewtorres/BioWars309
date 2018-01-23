b94739369783ac50bee01fb12826c14a419132c2
Krasios <sharonkuang97@gmail.com>
2018-01-22 18:52:18 -0800

    Ticket #10 added progressbar, tableView; reformatted resource boosts

--- src/client/gameplay/GamePlayController.java
+++ src/client/gameplay/GamePlayController.java
@@ -2,9 +2,14 @@ package client.gameplay;
 
 import client.ClientModel;
 import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
 
 public class GamePlayController {
 	ClientModel model;
+	@FXML
+	private ProgressBar virusProg;
+	@FXML 
+	private ProgressBar cureProg;
 	
 	public GamePlayController(ClientModel model) {
 		this.model = model;
@@ -12,6 +17,7 @@ public class GamePlayController {
 	
 	@FXML
 	private void initialize() {
-		
+		virusProg.getStyleClass().add("hide");
+		cureProg.getStyleClass().add("hide");
 	}
 }
